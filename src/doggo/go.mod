287c1efe054ede03b56327c0853504645d73e1ae
Cristian Sepulveda <csep94@gmail.com>
2024-09-14 13:34:20 -0300

    init commit

---
+++ src/doggo/go.mod
@@ -0,0 +1,3 @@
+module github.com/csepant/doggo
+
+go 1.23.1
---
+++ src/doggo/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "os",
+  "os/exec",
+  "syscall"
+)
+
+
+func main() {
+  
+  cmd := exec.Command("/hello")
+  syscall.Chroot("/testroot")
+  cmd.Stdin = os.Stdin
+  cmd.Stdout = os.Stdout
+  cmd.Stderr = os,Stderr
+  cmd.Run()
+}
